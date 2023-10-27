88ea4870a9ae121ff4fe3d6247d971a0e964543a
DylanWalsh <dawals22@colby.edu>
2023-10-27 13:57:20 -0400

    Finish frontend, 404page not found functionality added

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,15 @@
 import {useParams} from 'react-router-dom';
 import articles from './article-content';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
     const params = useParams();
     const articleID = params.articleID;
     const article = articles.find(article => article.name === articleID);
+
+    if(!article) {
+        return <NotFoundPage />
+    }
 
     return (
         // React fragment, in react cannot return more than one top level element (heading and paragraph, so need to wrap it into a react fragment)
